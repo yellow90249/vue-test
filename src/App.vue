861c9847c94a4a1a6d538e0e68d221cce35d6499
Gary <yellow851126@gmail.com>
2022-04-20 16:30:36 +0800

    feat: copy test

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <h1>test</h1>
+  <button>click</button>
+  <input type="text" />
 </template>
 
 <script>
 export default {
-
-}
+  mounted() {
+    const button = document.querySelector('button');
+    const input = document.querySelector('input');
+    button.addEventListener('click', () => {
+      input.value = 'abc';
+      input.select();
+      document.execCommand('copy');
+    });
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
